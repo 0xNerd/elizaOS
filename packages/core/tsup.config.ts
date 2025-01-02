83aa66d51519e645d15f839316d161c08023255d
0xNerd <thejoker3077@gmail.com>
2025-01-02 13:00:38 -0500

    Bump package version

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -5,25 +5,37 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: true,
     clean: true,
-    format: ["esm", "iife"], // Ensure you're targeting CommonJS
+    format: ["esm"],
     platform: "browser",
     target: "es2020",
     bundle: true,
-    splitting: false,  // Add this for better code splitting
-    dts: true,       // Generate declaration files
+    splitting: false,
+    dts: true,
+    noExternal: ["*"],
     external: [
-        "dotenv", // Externalize dotenv to prevent bundling
-        "fs", // Externalize fs to use Node.js built-in module
-        "path", // Externalize other built-ins if necessary
-        "http",
-        "https",
-        // Add other modules you want to externalize
+        "fs",
+        "fs/promises",
+        "path",
+        "os",
+        "crypto",
+        "stream",
+        "buffer",
+        "node:path",
+        "node:fs",
+        "node:http",
+        "node:https",
+        "node:crypto",
+        "node:os",
+        "dotenv",
     ],
     define: {
+        global: "undefined",
         "process.env.USE_OPENAI_EMBEDDING": "true",
         "process.env.USE_OLLAMA_EMBEDDING": "false",
     },
     esbuildOptions(options) {
-        options.globalName = "@ineedtendies/elizaos-core";
+        options.globalName = "ineedtendiesElizaosCore";
+        options.mainFields = ["browser", "module", "main"];
+        options.conditions = ["browser", "import"];
     },
 });
